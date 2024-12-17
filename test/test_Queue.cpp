1a2e5805b754c74c2e1fbf6897ad5afc647bc66c
T3nBuckS <eugesha120@mail.ru>
2024-12-17 12:02:01 +0300

    fix

---
+++ test/test_Queue.cpp
@@ -0,0 +1,115 @@
+﻿#include "stack_queue.h"
+
+#include <gtest.h>
+
+TEST(Queue, can_instantiate_queue)
+{
+    ASSERT_NO_THROW(Queue<int> myQueue);
+}
+
+
+TEST(Queue, is_not_full_on_creation)
+{
+    Queue<int> myQueue;
+
+    EXPECT_FALSE(myQueue.isFull());
+}
+
+
+TEST(Queue, is_empty_on_creation)
+{
+    Queue<int> myQueue;
+
+    EXPECT_TRUE(myQueue.isEmpty());
+}
+
+TEST(Queue, throws_exception_when_popping_empty_queue)
+{
+    Queue<int> myQueue;
+
+    ASSERT_ANY_THROW(myQueue.pop());
+}
+
+TEST(Queue, capacity_is_two)
+{
+    Queue<int> myQueue;
+
+    EXPECT_EQ(2, myQueue.getVector_capacity());
+}
+
+TEST(Queue, correct_size_after_pushes)
+{
+    Queue<int> myQueue;
+
+    myQueue.push(10);
+
+    myQueue.push(20);
+
+    myQueue.push(30);
+
+    myQueue.push(40);
+
+    EXPECT_EQ(4, myQueue.size());
+}
+
+TEST(Queue, can_extract_element)
+{
+    Queue<int> myQueue;
+
+    myQueue.push(5);
+
+    myQueue.push(10);
+
+    myQueue.pop();
+
+    EXPECT_EQ(1, myQueue.size());
+}
+
+TEST(Queue, can_insert_element)
+{
+    Queue<int> myQueue;
+
+    myQueue.push(5);
+
+    myQueue.push(7);
+
+    EXPECT_EQ(7, myQueue.getBack());
+}
+
+
+TEST(Queue, front_is_correct)
+{
+    Queue<int> myQueue;
+
+    myQueue.push(100);
+
+    myQueue.push(200);
+
+    EXPECT_EQ(100, myQueue.getFront());
+}
+
+
+TEST(Queue, back_is_correct)
+{
+    Queue<int> myQueue;
+
+    myQueue.push(100);
+
+    myQueue.push(200);
+
+    EXPECT_EQ(200, myQueue.getBack());
+}
+
+
+TEST(Queue, can_fill_correctly)
+{
+    Queue<int> q;
+
+    q.push(8); q.push(10);
+
+    q.pop();
+
+    q.push(14);
+
+    EXPECT_EQ(0, q.getBackIndex());
+}
---
+++ test/test_Stack.cpp
@@ -0,0 +1,103 @@
+﻿#include "gtest.h"
+
+#include "stack_queue.h"
+
+TEST(Stack, can_be_instantiated)
+{
+    EXPECT_NO_THROW(Stack<int> stack);
+}
+
+TEST(Stack, reports_empty_status_correctly)
+{
+    Stack<int> stack;
+
+    EXPECT_TRUE(stack.isEmpty());
+}
+
+TEST(Stack, throws_exception_when_popping_empty_stack)
+{
+    Stack<int> stack;
+
+    EXPECT_ANY_THROW(stack.pop());
+}
+
+TEST(Stack, reports_full_status_correctly)
+{
+    Stack<int> stack;
+
+    stack.push(5);
+
+    stack.push(10);
+
+    EXPECT_TRUE(stack.isFull());
+}
+
+TEST(Stack, returns_correct_capacity)
+{
+    Stack<int> stack;
+
+    EXPECT_EQ(2, stack.getVector_capacity());
+}
+
+TEST(Stack, returns_correct_size)
+{
+    Stack<int> stack;
+
+    stack.push(3);
+
+    stack.push(7);
+
+    stack.push(5);
+
+    EXPECT_EQ(3, stack.size());
+}
+
+TEST(Stack, correctly_pops_elements)
+{
+    Stack<int> stack;
+
+    stack.push(7);
+
+    stack.push(9);
+
+    stack.push(5);
+
+    stack.pop();
+
+    EXPECT_EQ(2, stack.size());
+}
+
+TEST(Stack, correctly_pushes_elements)
+{
+    Stack<int> stack;
+
+    stack.push(3);
+
+    stack.push(11);
+
+    EXPECT_EQ(11, stack.top());
+}
+
+TEST(Stack, returns_top_element)
+{
+    Stack<int> stack;
+
+    stack.push(15);
+
+    stack.push(25);
+
+    EXPECT_EQ(25, stack.top());
+}
+
+TEST(Stack, resizes_capacity_when_needed)
+{
+    Stack<int> stack;
+
+    stack.push(4);
+
+    stack.push(4);
+
+    stack.push(4);
+
+    EXPECT_EQ(3 * 2 + 2, stack.getVector_capacity());
+}
